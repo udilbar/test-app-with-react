de1a49cf93224f2c347203a130e81b839abbe3dc
Ndn1618 <ndilbar1618@gmail.com>
2020-12-19 12:32:56 +0500

    Set disalbled true when answer was chosen

--- src/App.js
+++ src/App.js
@@ -8,46 +8,55 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [showScore, setShowScore] = useState(false)
 
-  const buttonOnChangeHandler = ( isCorrect ) => {
-    if (isCorrect) {
+  const buttonOnChangeHandler = (evt) => {
+    if (evt.target.attributes.iscorrect.value === 'true') {
       setScore(score + 1)
+      evt.target.classList.add('correct')
+    } else {
+      evt.target.classList.add('incorrect')
     }
 
-
+    for (let index = 1; index <= 3; index++) {
+      evt.target.parentNode.children[index].disabled = true
+    }
 
     const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < Questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
+    if (nextQuestion < Questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
   };
 
   return (
 
-      <div className='container'>
+    <div className='container'>
       {
-          showScore ? (
-            <div className='score-section'>
-					    You scored {score} out of {Questions.length}
-				    </div>
-          ) : (
-            <>
-        <ul className='quiz-list'>
-        {
-            <li className='quiz-item'>
-              <p>{currentQuestion + 1}. {Questions[currentQuestion].questionText}</p>
-              {
-                Questions[currentQuestion].answerOptions.map((answerOption, id) => (
-                  <button key={id} id='quiz-button' type='button' onClick={buttonOnChangeHandler.bind(null, answerOption.isCorrect)}>{answerOption.option}. {answerOption.answerText}</button>
-                ))
-              }
-            </li>
-        }
-        </ul>
+        <>
+          <ul className='quiz-list'>
+            {
+              Questions.map((question, index) => {
+                return <li className='quiz-item' key={index}>
+                  <p>{index + 1}. {question.questionText}</p>
+                  {
+                    question.answerOptions.map((answerOption, id) => (
+                      <button iscorrect={String(answerOption.isCorrect)} key={id} id='quiz-button' type='button' disabled={false} onClick={buttonOnChangeHandler}>{answerOption.option}. {answerOption.answerText}</button>
+                    ))
+                  }
+                </li>
+              })
+            }
+          </ul>
+          {
+            showScore && (
+              <div className='score-section'>
+                You scored {score} out of {Questions.length}
+              </div>
+            )
+          }
         </>
-          )}
-      </div>
+      }
+    </div>
   );
 }
 
